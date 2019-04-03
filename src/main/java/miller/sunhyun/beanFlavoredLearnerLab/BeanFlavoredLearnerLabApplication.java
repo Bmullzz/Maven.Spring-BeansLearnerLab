a0ac818e3efc3c6ad65cba3b12708af3779ab43e
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-04-03 18:00:46 -0400

    first commit

---
+++ src/main/java/miller/sunhyun/beanFlavoredLearnerLab/BeanFlavoredLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package miller.sunhyun.beanFlavoredLearnerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanFlavoredLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanFlavoredLearnerLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miller/sunhyun/beanFlavoredLearnerLab/BeanFlavoredLearnerLabApplicationTests.java
@@ -0,0 +1,16 @@
+package miller.sunhyun.beanFlavoredLearnerLab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeanFlavoredLearnerLabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
